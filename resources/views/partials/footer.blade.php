8169a9322b9298a5abe3dd0daea70cd99fd274f7
SulivanM <sulivan.michel@switchcompagnie.eu>
2025-07-26 12:18:59 +0200

    Update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,8 +10,8 @@
       </li>
     </ul>
     <p>Made In {{ now()->year }}. Ruby Realms Studios [BY SWITCH COMPAGNIE] - Revive Forever.</p>
-    <a class="footer-logo" href="#" class="th">
-      <i class="fa-solid fa-users"></i>
+    <a href="#" class="footer-logo th inline-block">
+      <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" class="w-10 h-10 rounded-full object-cover" />
     </a>
   </div>
 </div>
